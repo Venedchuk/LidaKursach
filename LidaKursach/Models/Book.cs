1e7d155d2ab12fa7226e9ec0a8fe94ffa73da92a
METEOR\andrii <sneyp67@gmail.com>
2017-03-17 17:03:21 +0200

    add model db

--- LidaKursach/Models/Book.cs
+++ LidaKursach/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetIdentityApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -8,15 +9,39 @@ namespace LidaKursach.Models
 {
     public class Book
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
-        public decimal Price { get; set; }
+        public virtual Genres Genre { get; set; }
+        public int Count { get; set; }
     }
+    public class Genres
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+    public class Authors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Biography { get; set; }
+    }
+    public class Carts
+    {
+        public int Id { get; set; }
+        public DateTime Start_reading { get; set; }
+        public DateTime? FinishReading { get; set; }
+        public string Status { get; set; }
+        public virtual Book Book_Id { get; set; }
+        public virtual RegisterModel User_Id { get; set; }
+    }
+
+
 
     public class MovieDBContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+        public DbSet<Genres> Genres { get; set; }
     }
 }
